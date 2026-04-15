8a3e7023ead9c749b760e2f93e38b2fa74281677
Ashfaq <113580367+AshfaqIbrahim@users.noreply.github.com>
2026-04-15 13:50:58 +0530

    fs module second commit

--- TelWorks/Crud.js
+++ TelWorks/Crud.js
@@ -1,36 +1,48 @@
 const fs = require("fs");
 
-// 1.Write File
-//Sync
-fs.writeFileSync("./TelWorks/test.txt", "Hey Iam Synchronous");
+// // 1.Write File
+// //Sync
+// fs.writeFileSync("./TelWorks/test.txt", "Hey Iam Synchronous");
 
-//Async
-fs.writeFile("./TelWorks/test.txt", "Hello, This is a test", (err) => {});
+// //Async
+// fs.writeFile("./TelWorks/test.txt", "Hello, This is a test", (err) => {});
 
-// 2.Read File
-//Sync
-const result = fs.readFileSync("./TelWorks/contacts.txt", "utf-8");
-console.log(result);
+// // 2.Read File
+// //Sync
+// const result = fs.readFileSync("./TelWorks/contacts.txt", "utf-8");
+// console.log(result);
 
-//Async
-fs.readFile("./TelWorks/contacts.txt", "utf-8", (err, result) => {
-  if (err) {
-    console.log("Error: ", err);
-  } else {
-    console.log(result);
-  }
-});
+// //Async
+// fs.readFile("./TelWorks/contacts.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log("Error: ", err);
+//   } else {
+//     console.log(result);
+//   }
+// });
 
-// 3.Apending File
-//Sync
-fs.appendFileSync("./TelWorks/test.txt", "\nHey there I got appended");
+// // 3.Apending File
+// //Sync
+// fs.appendFileSync("./TelWorks/test.txt", "\nHey there I got appended");
 
-//Async
-fs.appendFile(
-  "./TelWorks/test.txt",
-  "\nHey there I got appended and Im Asynchronous",
-  (err) => {},
-);
+// //Async
+// fs.appendFile(
+//   "./TelWorks/test.txt",
+//   "\nHey there I got appended and Im Asynchronous",
+//   (err) => {},
+// );
 
-//4. Delete File
-fs.unlinkSync("./TelWorks/todelete.txt"); //todelete.txt file is deleted
+// //4. Delete File
+// fs.unlinkSync("./TelWorks/todelete.txt"); //todelete.txt file is deleted
+
+// //ALl together
+// fs.writeFile("./test2.txt", "Hey there", (err) => {});
+// fs.readFile("./test2.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(result);
+//   }
+// });
+// fs.appendFile("./test2.txt", "\nHeyloooo", (err) => {});
+// fs.unlink("./test2.txt", (err) => {});
