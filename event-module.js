eb80a02db669a5c2fc8cabed81c97a4f33aed436
Ashfaq <113580367+AshfaqIbrahim@users.noreply.github.com>
2026-04-24 11:49:28 +0530

    for using gitignore

--- event-module.js
+++ event-module.js
@@ -17,31 +17,76 @@
 // //Triggering an event,
 // event.emit("order-pizza", "large", "extra cheese");
 
-//Simple calculator using event module
-const EventEmitter = require("events");
-const event = new EventEmitter();
+// //once()
+// const EventEmitter = require("events");
+// const event = new EventEmitter();
 
-event.on("add", (a, b) => {
-  console.log("Addition:", a + b);
-});
+// //once()
+// event.once("login", () => {
+//   console.log("Logged in");
+// });
 
-event.on("sub", (a, b) => {
-  console.log("subtraction:", a - b);
-});
+// event.emit("login");
+// event.emit("login"); //This wont run this time, because it used once(), once() runs only for one time
 
-event.on("mul", (a, b) => {
-  console.log("Multiplication:", a * b);
-});
+// //off()
+// const EventEmitter = require("events");
+// const event = new EventEmitter();
 
-event.on("div", (a, b) => {
-  if (b === 0) {
-    console.log("Cannot divide by zero");
-  } else {
-    console.log("Division:", a / b);
-  }
-});
+// const greet = () => {
+//   console.log("Hello");
+// };
 
-event.emit("add", 5, 2);
-event.emit("sub", 5, 2);
-event.emit("mul", 5, 2);
-event.emit("div", 5, 2);
+// event.on("Hey", greet);
+// event.off("Hey", greet); //Removes the "Hey" listener
+
+// event.emit("Hey");
+
+// //removeAllListeners(event)
+// const EventEmitter = require("events");
+// const event = new EventEmitter();
+
+// event.on("test", () => {
+//   console.log("A");
+// });
+// event.on("test", () => {
+//   console.log("B");
+// });
+// event.on("test", () => {
+//   console.log("C");
+// });
+
+// event.removeAllListeners("test"); //Remove every 'test' listener
+
+// event.emit("test"); //Nothing will run
+
+
+// //Simple calculator using event module
+// const EventEmitter = require("events");
+// const event = new EventEmitter();
+
+// //on
+// event.on("add", (a, b) => {
+//   console.log("Addition:", a + b);
+// });
+
+// event.on("sub", (a, b) => {
+//   console.log("subtraction:", a - b);
+// });
+
+// event.on("mul", (a, b) => {
+//   console.log("Multiplication:", a * b);
+// });
+
+// event.on("div", (a, b) => {
+//   if (b === 0) {
+//     console.log("Cannot divide by zero");
+//   } else {
+//     console.log("Division:", a / b);
+//   }
+// });
+
+// event.emit("add", 5, 2);
+// event.emit("sub", 5, 2);
+// event.emit("mul", 5, 2);
+// event.emit("div", 5, 2);
